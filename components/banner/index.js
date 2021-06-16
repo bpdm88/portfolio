90c29c1e5bb9a35b620a5a870f4d2554dd46d288
Ben <benjamin.main88@gmail.com>
2021-06-16 18:03:22 +0100

    cogs added to banner

--- components/banner/index.js
+++ components/banner/index.js
@@ -1,13 +1,31 @@
+import Button from '../button/primary'
+
 const Banner = () => {
   return (
     <div className="banner">
       <div className="welcome-container">
-        <h1 className="heading--alpha slide-in-title">Hello I'm Ben</h1>
-        <div className="welcome-subsection slide-in-subtitle">
-          <p className="heading--bravo">
+        <div className="welcome-title">
+          <h1 className="heading--alpha slide-in-title">Hello I'm Ben</h1>
+        </div>
+        <div className="welcome-subsection">
+          <p className="slide-in-subtitle heading--bravo">
             I'm a software developer from Bristol who enjoys building great user experiences with
             clean and efficent code.
           </p>
+          <div className="slide-in-btn">
+            <Button>Learn More</Button>
+          </div>
+        </div>
+      </div>
+      <div>
+        <div className="cog-1 rotate">
+          <img src="/assets/img/cogs/cog1.svg" />
+        </div>
+        <div className="cog-2 reverse">
+          <img src="/assets/img/cogs/cog2.svg" />
+        </div>
+        <div className="cog-3 rotate">
+          <img src="/assets/img/cogs/cog3.svg" />
         </div>
       </div>
     </div>
