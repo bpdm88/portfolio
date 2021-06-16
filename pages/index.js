5651bfe7262e169848028219f68ef2708223cd24
Ben <benjamin.main88@gmail.com>
2021-06-16 09:01:28 +0100

    Banner added

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 
 import Header from '../components/header'
+import Banner from '../components/banner'
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
       </Head>
 
       <Header />
+      <Banner />
     </div>
   )
 }
