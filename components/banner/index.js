67cbdc72973534eb9084422f551a0fc0cff424b4
Ben <benjamin.main88@gmail.com>
2021-06-16 14:16:16 +0100

    banner animations

--- components/banner/index.js
+++ components/banner/index.js
@@ -1,5 +1,17 @@
 const Banner = () => {
-  return <div className="banner"></div>
+  return (
+    <div className="banner">
+      <div className="welcome-container">
+        <h1 className="heading--alpha slide-in-title">Hello I'm Ben</h1>
+        <div className="welcome-subsection slide-in-subtitle">
+          <p className="heading--bravo">
+            I'm a software developer from Bristol who enjoys building great user experiences with
+            clean and efficent code.
+          </p>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Banner
