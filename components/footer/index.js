d7c37eacf58c1f191813a97cbea01e3d5f043197
Ben <benjamin.main88@gmail.com>
2021-07-03 18:59:08 +0100

    footer edits

--- components/footer/index.js
+++ components/footer/index.js
@@ -5,9 +5,15 @@ const Footer = () => {
         <img src="/assets/img/icons/bm-logo-white.svg" className="footer-logo" />
       </div>
       <div className="footer-socials">
-        <img src="/assets/img/icons/linkedin.svg" className="footer-social" />
-        <img src="/assets/img/icons/gmail.svg" className="footer-social" />
-        <img src="/assets/img/icons/github.svg" className="footer-social" />
+        <a href="https://www.linkedin.com/in/benjamin-main-84b2a2bb/" target="blank">
+          <img src="/assets/img/icons/linkedin.svg" className="footer-social" />
+        </a>
+        <a href="mailto:benjamin.main88@gmail.com" target="blank">
+          <img src="/assets/img/icons/gmail.svg" className="footer-social" />
+        </a>
+        <a href="https://github.com/bpdm88" target="blank">
+          <img src="/assets/img/icons/github.svg" className="footer-social" />
+        </a>
       </div>
     </div>
   )
